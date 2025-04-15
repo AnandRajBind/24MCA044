45b5d8eb9a4b68e34b5e3fe910ab32128430b47a
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-04-15 19:53:45 +0530

    15/04/2025
    Setup System

---
+++ Assesment/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-amber-400 text-2xl'>Anand Raj </h1>
+    </>
+  )
+}
+
+export default App
---
+++ oops/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("This is a simple Java program.");
+    System.out.println("It demonstrates basic syntax and structure.");
+    System.out.println("Feel free to modify and experiment with the code.");
+    System.out.println("Happy coding!");
+}
+}
